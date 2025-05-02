43200f05496a9c6e0799361af8183a793d1b4d2c
shn73 <shn-73@mail.ru>
2025-05-02 20:01:38 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,9 @@
 def mask_account_card(account_card: str) -> str:
     """функция которая обрабатывауе информацию как о картах, так и о счетах"""
     if "Счет" in account_card:
-        return f"{account_card [:5]} **{account_card[-20: -16]}"
+        return f"{account_card[:5]} **{account_card[-20: -16]}"
     else:
-        return f"{account_card [:-16]}{account_card [-16:-12]} {account_card [-12:-10]}** **** {account_card[-4:]}"
+        return f"{account_card[:-16]}{account_card[-16:-12]} {account_card[-12:-10]}** **** {account_card[-4:]}"
 
 
-
-print(mask_account_card(account_card = 'Visa Platinum 7000792289606361'))
+print(mask_account_card(account_card='Visa Platinum 7000792289606361'))
