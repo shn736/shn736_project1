524c3017c9303ae6655cc03617beeb7e89cf2f7f
shn73 <shn-73@mail.ru>
2025-05-02 20:13:21 +0300

    fixes

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from src.masks import get_mask_card_number, get_mask_account
 
+from src.widget import mask_account_card, get_date
+
 print(get_mask_card_number(card_number='7000792289606361'))
 
 print(get_mask_account(account='73654108430135874305'))
+
+print(mask_account_card(account_card='Visa Platinum 7000792289606361'))
+
+print(get_date(date='2024-03-11T02:26:18.671407'))
--- src/widget.py
+++ src/widget.py
@@ -6,12 +6,12 @@ def mask_account_card(account_card: str) -> str:
         return f"{account_card[:-16]}{account_card[-16:-12]} {account_card[-12:-10]}** **** {account_card[-4:]}"
 
 
-print(mask_account_card(account_card='Visa Platinum 7000792289606361'))
+mask_account_card(account_card='Visa Platinum 7000792289606361')
 
 
 def get_date(date: str) -> str:
     """возвращает строку с датой в формате ДД.ММ.ГГГГ"""
-    return f" {date[8: 10]}.{date[5: 7]}.{date[0: 4]}"
+    return f"{date[8: 10]}.{date[5: 7]}.{date[0: 4]}"
 
 
-print(get_date(date='2024-03-11T02:26:18.671407'))
+get_date(date='2024-03-11T02:26:18.671407')
