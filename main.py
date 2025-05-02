efc15aad0e619171bae87025e8bc66062b923d22
shn73 <shn-73@mail.ru>
2025-05-02 19:39:38 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+print(get_mask_card_number(card_number='7000792289606361'))
+
+print(get_mask_account(account='73654108430135874305'))
---
+++ src/masks.py
@@ -0,0 +1,22 @@
+def get_mask_card_number(card_number: str) -> str:
+    """принимает на вход номер карты и возвращает ее маску"""
+    if len(card_number) != 16:
+        return "Вы ввели не верный номер карты"
+
+    else:
+        return f"{card_number[0: 4]} {card_number[4: 6]}** ****  {card_number[-4:]}"
+
+
+get_mask_card_number(card_number='7000792289606361')
+
+
+def get_mask_account(account: str) -> str:
+    """принимает на вход номер счета и возвращает его маску"""
+    if len(account) != 20:
+        print("Вы ввели не верный номер счета")
+
+    else:
+        return f"**{account[-4:]}"
+
+
+get_mask_account(account='73654108430135874305')
