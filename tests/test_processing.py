d7357d22bdcc752bf178c43ec5b6eba111f8596b
shn73 <shn-73@mail.ru>
2025-05-20 10:27:09 +0300

    network 10.1 change 8 tests 3

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -27,3 +27,31 @@ def test_filter_by_state_canceled(filter_state):
         {'date': '2018-10-14T08:21:33.419441', 'id': 615064591, 'state': 'CANCELED'},
         {'date': '2018-09-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'},
         {'date': '2018-10-14T08:21:33.419441', 'id': 615064591, 'state': 'CANCELED'}]
+
+
+@pytest.fixture
+def sort_date():
+    return [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+
+
+def test_sort_by_date_true(sort_date):
+    assert sort_by_date(sort_date, reverse=True) == [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
+
+
+def test_sort_by_date_false(sort_date):
+    assert sort_by_date(sort_date, reverse=False) == [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    ]
