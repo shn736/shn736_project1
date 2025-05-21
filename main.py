73c333f38bffe579f6efcc44e08dd7ab752c167f
shn73 <shn-73@mail.ru>
2025-05-21 12:49:43 +0300

    network 10.1 change 14

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ print(get_mask_card_number('7000792289606361'))
 
 print(get_mask_account('73654108430135874305'))
 
-print(mask_account_card(account_card='Счет 73654108430135874305'))
+print(mask_account_card('Счет 73654108430135874305'))
 
-print(get_date(date='2024-03-11T02:26:18.671407'))
+print(get_date('2024-03-11T02:26:18.671407'))
 
 print(filter_by_state(transactions=[
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
