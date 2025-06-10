e550e055db7167fa0ac78656612b526fd5e106c0
shn73 <shn-73@mail.ru>
2025-06-10 11:48:33 +0300

    homework_12_2 change_2

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,15 @@
 import json
 from typing import Any
+import logging
+
+
+logging.basicConfig(encoding='utf-8')
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/utils.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def operations_list(path: str) -> Any:
@@ -7,8 +17,11 @@ def operations_list(path: str) -> Any:
     try:
         with open(path, encoding='utf-8') as json_file:
             operation_str = json.load(json_file)
+            logger.info('Возвращен список словарей')
             return operation_str
     except json.JSONDecodeError:
+        logger.error('Неправильная структура JSON')
         return []
     except FileNotFoundError:
+        logger.error('Файл не существует в указанном месте')
         return []
