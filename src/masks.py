966f674ab117dc2569833779d5542a4d87cdf150
shn73 <shn-73@mail.ru>
2025-06-10 13:28:03 +0300

    homework_12_2 change_3

--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(encoding='utf-8')
 logger = logging.getLogger('masks')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler('logs/masks.log')
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
